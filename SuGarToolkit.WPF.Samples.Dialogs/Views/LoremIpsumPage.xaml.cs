58240c9171a7b48c1ab23067e6aeb49f6d9164c6
Sky_SuGar <wxk0218@icloud.com>
2025-08-25 22:19:08 +0800

    Correct the namespace

---
+++ SuGarToolkit.WPF.Samples.Dialogs/Views/LoremIpsumPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace SuGarToolkit.WPF.Samples.Dialogs.Views;
+
+public partial class LoremIpsumPage : Page
+{
+    public LoremIpsumPage()
+    {
+        InitializeComponent();
+    }
+}
