fdfe540f1054f925942dbed29e40a9853cbbaa64
Sky_SuGar <wxk0218@icloud.com>
2025-08-31 20:36:22 +0800

    Refactor DependencyPropertyGenerator; Add RelayDependencyPropertyGenerator.

---
+++ SuGarToolkit.WPF.SourceGenerators.DependencyPropertyGenerator/DependencyPropertyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace SuGarToolkit.WPF.SourceGenerators
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public sealed class DependencyPropertyAttribute : Attribute
+    {
+        public object DefaultValue { get; set; }
+
+        public string DefaultValuePath { get; set; }
+
+        //public PropertyChangedCallback OnPropertyChanged { get; set; }
+    }
+}
---
+++ SuGarToolkit.WPF.SourceGenerators.RelayDependencyPropertyGenerator/RelayDependencyPropertyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SuGarToolkit.WPF.SourceGenerators
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public sealed class RelayDependencyPropertyAttribute : Attribute
+    {
+        public RelayDependencyPropertyAttribute(string TargetPropertyPath)
+        {
+            _path = TargetPropertyPath;
+        }
+
+        public object DefaultValue { get; set; }
+
+        public string DefaultValuePath { get; set; }
+
+        private readonly string _path;
+    }
+}
